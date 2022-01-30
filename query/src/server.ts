cc162109bf5dd71f51a84e9053a2639b6f467a26
dalecs <dale.seen@gmail.com>
2022-01-30 15:12:42 -0800

    Defined query service

--- query/src/server.ts
+++ query/src/server.ts
@@ -6,6 +6,16 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+// @method GET /posts
+// @desc Query posts and their comments
+// @access public
+app.get("/posts", async (req: Request, res: Response): Promise<void> => {});
+
+// @method POST /events
+// @desc Receive events
+// @access public
+app.post("/events", async (req: Request, res: Response): Promise<void> => {});
+
 const PORT = process.env.PORT ?? 4002;
 app.listen(PORT, (): void => {
   console.log(`Query service running on port: ${PORT}`);
